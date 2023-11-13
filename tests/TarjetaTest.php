7f9711b783e0c0a82f8f634466d5a751bdaa30cc
JeronimoLedesma <ledesmajero@gmail.com>
2023-11-12 23:38:19 -0300

    Probando tests, arreglos generales

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -7,7 +7,8 @@ class TajetaTest extends TestCase{
     public function testPagarParcial(){
         $tarjeta = new FranquiciaParcial (120, 46210);
         $cole = new Colectivo (73);
-        $cole->pagarCon($tarjeta);
+        $tiempo = new TiempoFake();
+        $cole->pagarCon($tarjeta. $tiempo);
         $this->assertEquals($tarjeta->getSaldo(), 60);
     }
 }
