790f421c252bdb1c1d53993e3419856bdc2eaa84
JeronimoLedesma <ledesmajero@gmail.com>
2023-11-13 07:39:15 -0300

    Primer test

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -8,6 +8,7 @@ class TarjetaTest extends TestCase{
     
     public function testCarga(){
     $tarjeta = new Tarjeta(0);
+    $this->assertEquals($tarjeta->getSaldo(), 0);
     $tarjeta->cargarSaldo(6700);
     $this->assertEquals($tarjeta->saldo, 0);
     $this->assertEquals($tarjeta->cargarSaldo(300), 300);
