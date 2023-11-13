9028177e676f708a2ddd54d133cd1d63886f34af
JeronimoLedesma <ledesmajero@gmail.com>
2023-11-13 10:42:27 -0300

    Franquicias

--- tests/ParcialTest.php
+++ tests/ParcialTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class ParcialTest extends TestCase{
 
     public function testParcial(){
-        $tarjeta = new FranquciaParcial(500);
+        $tarjeta = new FranquiciaParcial(500);
         $cole = new Colectivo(103);
         $cole->pagarCon($tarjeta);
         $this->assertEqual($tarjeta->getSaldo(), 440);
