19ade4e51e2a04caca43879757cb75727d7e30cb
JeronimoLedesma <ledesmajero@gmail.com>
2023-11-13 11:14:24 -0300

    Franquicias

--- src/FranquiciaCompleta.php
+++ src/FranquiciaCompleta.php
@@ -3,8 +3,6 @@ namespace TrabajoSube;
 use TrabajoSube\Tarjeta;
 class FranquiciaCompleta extends Tarjeta
 {
-    public $costoBoleto;
-    public $saldo;
     public function __construct($saldo){
         $this->saldo = $saldo;
         $this->costoBoleto = 0;
--- src/FranquiciaParcial.php
+++ src/FranquiciaParcial.php
@@ -3,10 +3,7 @@ namespace TrabajoSube;
 use TrabajoSube\Tarjeta;
 
 class FranquiciaParcial extends Tarjeta{
-    
-    public $costoBoleto;
-    public $saldo;
-    
+
     public function __contruct($saldo){
         $this->saldo = $saldo;
         $this->costoBoleto = 60;
