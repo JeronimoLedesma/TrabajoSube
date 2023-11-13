964656b3b763302332d8dc39446e8b70f0d56b87
JeronimoLedesma <ledesmajero@gmail.com>
2023-11-13 06:16:10 -0300

    reiniciando

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -1,33 +1,16 @@
 <?php
 namespace TrabajoSube;
 class Colectivo{
-    protected $linea;
     
-    public function __construct($linea){
-        $this->linea = $linea;
-    }
+protected $linea;
     
-    //    Funcion de ejemplo para test
-    public function getLinea(){
-        return $this->linea;
-    }
+public function __construct($linea){
+    $this->linea = $linea;
+}
+    
+//    Funcion de ejemplo para test
+public function getLinea(){
+    return $this->linea;
+}
 
-    public function pagarCon($tarjeta, TiempoFake $hora){
-        $abono = $tarjeta->saldoExtra;
-        if ($tarjeta->saldo - $tarjeta->costoBoleto >= -211.84){
-            if ($tarjeta->tipo == "regular") {
-                $tarjeta->bajarsaldo();
-            }
-            else {
-                $tarjeta->bajarsaldo($hora->tiempo);
-            }
-            $boleto = new Boleto($tarjeta->saldo, $this->linea, $tarjeta->tipo, $tarjeta->ID, ($abono - $tarjeta->saldoExtra));
-            $boleto->muestra_boleto();
-            return true;
-        }
-        else{
-            echo "Saldo insuficiente y no le quedan oportunidades de viaje plus, por favor recargue su tarjeta";
-            return false;
-        }
-    }
 }
