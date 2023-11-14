3171bcc76747b4c3a3db9933dc12cee3946e7092
JeronimoLedesma <ledesmajero@gmail.com>
2023-11-14 13:24:55 -0300

    arreglo parcial (de vuelta al 4)

--- src/FranquiciaParcial.php
+++ src/FranquiciaParcial.php
@@ -24,7 +24,7 @@ class FranquiciaParcial extends Tarjeta{
         }
 
         if($this->saldo - $cantidad >= -211.84 && $this->viajePlus < 2 && (time()-$this->momentoPago) >= 300){
-            if($this->viajesHoy < 3){
+            if($this->viajesHoy < 4){
                 $this->viajesHoy ++;
             }
             else{
