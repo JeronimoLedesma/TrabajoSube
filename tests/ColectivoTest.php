8d5715255f8915642be3088e6c81ca07e091b719
JeronimoLedesma <ledesmajero@gmail.com>
2023-11-14 10:19:45 -0300

    Mejora de tests

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -13,30 +13,7 @@ class ColectivoTest extends TestCase{
 
     public function testPagar(){
         $cole = new Colectivo(103);
-        $tarjeta = new Tarjeta(80,46216);
-        $cole->pagarCon($tarjeta);
-        $this->assertEquals($tarjeta->getSaldo(), -40);
-        $this->assertEquals($tarjeta->viajePlus, 1);
-        $cole->pagarCon($tarjeta);
-        $this->assertEquals($tarjeta->getSaldo(), -160);
-        $this->assertEquals($tarjeta->viajePlus, 2);
-        $this->assertEquals($cole->pagarCon($tarjeta), false);
-        $tarjeta->cargarSaldo(200);
-        $this->assertEquals($tarjeta->getSaldo(), 40);
-        $this->assertEquals($tarjeta->viajePlus, 0);
-    }
-
-    public function testRecargaSobrante(){
-        $cole = new Colectivo(103);
-        $tarjeta = new Tarjeta(6600, 46216);
-        $tarjeta->cargarSaldo(220);
-        $this->assertEquals($tarjeta->getSaldo(), 6600);
-        $this->assertEquals($tarjeta->saldoSobrante,220);
-        $cole->pagarCon($tarjeta);
-        $this->assertEquals($tarjeta->saldoSobrante,100);
-        $this->assertEquals($tarjeta->getSaldo(), 6600);
-        $cole->pagarCon($tarjeta);
-        $this->assertEquals($tarjeta->saldoSobrante, 0);
-        $this->assertEquals($tarjeta->getSaldo(), 6580);
+        $tarjeta = new Tarjeta(800,46216);
+        $this->assertEquals($tarjeta->getSaldo(), 680);
     }
 }
--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -6,13 +6,45 @@ use PHPUnit\Framework\TestCase;
 
 class TarjetaTest extends TestCase{
     
-    public function testCarga(){
+    public function testCargaSobrante(){
     $tarjeta = new Tarjeta(0, 46216);
     $this->assertEquals($tarjeta->getSaldo(), 0);
     $tarjeta->cargarSaldo(6700);
     $this->assertEquals($tarjeta->getSaldo(), 6600);
     $this->assertEquals($tarjeta->saldoSobrante, 100);
-    $this->assertEquals($tarjeta->cargarSaldo(5), false);
+    }
 
+    public function testCargarfallo(){
+    $tarjeta = new Tarjeta(0, 46216);
+    $this->assertEquals($tarjeta->cargarSaldo(5), false);
+    }
+
+    public function testViajePlus(){
+        $cole = new Colectivo(103);
+        $tarjeta = new Tarjeta(80,46216);
+        $cole->pagarCon($tarjeta);
+        $this->assertEquals($tarjeta->getSaldo(), -40);
+        $this->assertEquals($tarjeta->viajePlus, 1);
+        $cole->pagarCon($tarjeta);
+        $this->assertEquals($tarjeta->getSaldo(), -160);
+        $this->assertEquals($tarjeta->viajePlus, 2);
+        $this->assertEquals($cole->pagarCon($tarjeta), false);
+        $tarjeta->cargarSaldo(200);
+        $this->assertEquals($tarjeta->getSaldo(), 40);
+        $this->assertEquals($tarjeta->viajePlus, 0);
+    }
+
+    public function testRecargaSobrante(){
+        $cole = new Colectivo(103);
+        $tarjeta = new Tarjeta(6600, 46216);
+        $tarjeta->cargarSaldo(220);
+        $this->assertEquals($tarjeta->getSaldo(), 6600);
+        $this->assertEquals($tarjeta->saldoSobrante,220);
+        $cole->pagarCon($tarjeta);
+        $this->assertEquals($tarjeta->saldoSobrante,100);
+        $this->assertEquals($tarjeta->getSaldo(), 6600);
+        $cole->pagarCon($tarjeta);
+        $this->assertEquals($tarjeta->saldoSobrante, 0);
+        $this->assertEquals($tarjeta->getSaldo(), 6580);
     }
 }
