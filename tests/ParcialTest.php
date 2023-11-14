85733e2d18249d472c5fcced93b0e341ab321c76
JeronimoLedesma <ledesmajero@gmail.com>
2023-11-14 13:08:43 -0300

    arreglo ParcialTest

--- tests/ParcialTest.php
+++ tests/ParcialTest.php
@@ -19,21 +19,27 @@ class ParcialTest extends TestCase{
         $this->assertEquals($tarjeta->reducirSaldo($tarjeta->costoBoleto), true);
         $this->assertEquals($tarjeta->getSaldo(), 440);
         $this->assertEquals($tarjeta->viajesHoy, 1);
-        $this->assertEquals($tarjeta->reducirSaldo($tarjeta->costoBoleto), false);
+        $tarjeta->momentoPago = time()-300;
+        $this->assertEquals($tarjeta->reducirSaldo($tarjeta->costoBoleto), true);
         $this->assertEquals($tarjeta->getSaldo(), 380);
         $this->assertEquals($tarjeta->viajesHoy, 2);
+        $tarjeta->momentoPago = time()-300;
         $this->assertEquals($tarjeta->reducirSaldo($tarjeta->costoBoleto), true);
         $this->assertEquals($tarjeta->getSaldo(), 320);
         $this->assertEquals($tarjeta->viajesHoy, 3);
+        $tarjeta->momentoPago = time()-300;
         $this->assertEquals($tarjeta->reducirSaldo($tarjeta->costoBoleto), true);
         $this->assertEquals($tarjeta->getSaldo(), 260);
         $this->assertEquals($tarjeta->viajesHoy, 4);
+        $tarjeta->momentoPago = time()-300;
         $this->assertEquals($tarjeta->reducirSaldo($tarjeta->costoBoleto), true);
         $this->assertEquals($tarjeta->getSaldo(), 140);
         $tarjeta->ultimoDiaViaje = strtotime("sunday");
+        $tarjeta->momentoPago = time()-300;
         $this->assertEquals($tarjeta->reducirSaldo($tarjeta->costoBoleto), true);
         $this->assertEquals($tarjeta->getSaldo(), 80);
         $this->assertEquals($tarjeta->viajesHoy, 0);
+        $this->assertEquals($tarjeta->reducirSaldo($tarjeta->costoBoleto), false);
     
     }
 }
